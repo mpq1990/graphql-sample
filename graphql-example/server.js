ace78ebcf4ac0e84634770ef8b5f20c8014c8036
Majid Qureshi <majid.qureshi@clark.de>
2022-04-17 14:15:41 +0200

    feat: sample graphql

---
+++ graphql-example/server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { loadFilesSync } = require("@graphql-tools/load-files");
+
+const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
+
+const schema = makeExecutableSchema({
+  typeDefs: typesArray,
+  resolvers: resolversArray
+});
+
+const app = express();
+
+app.use("/graphql", graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(3000, () => {
+  console.log("Running graphql server on 3000....")
+});
